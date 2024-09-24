bf40fa2dedbc028d74d9102a5177ce8c4c856d5c
Paul Lieber <paullieber07@gmail.com>
2024-09-23 22:09:03 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,3 @@
-// JavaScript from index.html
-
-// JavaScript code to change the image when clicked
-const images = [
-    'images/image_0.png',
-    'images/image_1.png',
-    'images/image_2.png',
-    'images/image_3.png',
-    'images/image_4.png',
-    'images/image_5.png',
-    'images/image_6.png',
-    'images/image_7.png',
-    'images/image_8.png',
-    'images/image_9.png'
-];
-
-const imgElement = document.getElementById('bathroom-image');
-
-imgElement.addEventListener('click', () => {
-    // Generate a random index
-    const randomIndex = Math.floor(Math.random() * images.length);
-    // Set the image source to the new random image
-    imgElement.src = images[randomIndex];
-});
-
-
-
 // scripts/script.js
 
 // Array of image paths
@@ -68,4 +41,3 @@ imgElement.addEventListener('click', generateRandomImage);
 
 // Generate the first random image on page load
 window.onload = generateRandomImage;
-
